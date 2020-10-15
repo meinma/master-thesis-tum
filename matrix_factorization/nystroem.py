* adjusted RMSE computation and added some documentation for helpers.py * added documentation for nystroem.py * fixed typos in experiments.py and changed time measurement

matrix_factorization/nystroem.py
@@ -2,6 +2,7 @@ import os
 
 import h5py
 import numpy as np
+import scipy
 import torch
 
 from utils import load_kern, constructSymmetricMatrix
@@ -9,6 +10,14 @@ from utils import load_kern, constructSymmetricMatrix
 
 class Nystroem:
     def __init__(self, n_components, k, dataset, model, path):
+        """
+        Initialize Nyström solver
+        @param n_components: number of m data points for which the kernel is evaluated exactly in matrix W (called M in the thesis)
+        @param k: is used for the SVD only the first k most important eigenvalues are kept, others are set to zero
+        @param dataset: dataset for which the nyström method is used
+        @param model: model defining the kernel between two training data points
+        @param path: matrices are stored there (requires to be a h5 file)
+        """
         torch.cuda.empty_cache()
         self.n_components = n_components
         self.model = model
@@ -21,7 +30,7 @@ class Nystroem:
 
     def loadMatrices(self):
         """
-        @return: the matrix C and C_diag which have been computed before and stored as h5py file
+        @return: the matrix W and C_down which have been computed before and stored as h5py file
         """
         print("Loading")
         with h5py.File(self.path, "r") as f:
@@ -42,7 +51,8 @@ class Nystroem:
         W = constructSymmetricMatrix(W)
         # Stack both matrices row-wise
         C = np.vstack((W, C_down))
-        u, sigma, vt = np.linalg.svd(W, full_matrices=False)
+        u, sigma, vt = scipy.linalg.svd(W, full_matrices=False)
+        # u, sigma, vt = np.linalg.svd(W, full_matrices=False)
         u_k = u[:, :self.k]
         sigma_k = sigma[:self.k]
         vt_k = vt[:self.k, :]

utils/helpers.py
@@ -11,13 +11,15 @@ from scipy.sparse.linalg import lsmr
 from sklearn import metrics
 from torch.utils.data import DataLoader
 
+from utils.Error import Error
+
 sn.set()
 
-__all__ = ('createPlots', 'plotEigenvalues', 'oneHotEncoding', 'computeRMSE', 'compute_recall', 'compute_accuracy',
-           'computeMeanVariance', 'constructSymmetricIfNotSymmetric', 'compute_precision', 'computePredictions',
+__all__ = ('createPlots', 'plotEigenvalues', 'oneHotEncoding', 'computeRMSE', 'compute_accuracy',
+           'computeMeanVariance', 'constructSymmetricIfNotSymmetric', 'computePredictions',
            'constructSymmetricMatrix', 'isSymmetric', 'print_accuracy', 'load_kern', 'loadTargets', 'deleteDataset',
-           'solve_system_old', 'deleteValues', 'diag_add', 'generateSquareRandomMatrix',
-           'solve_system', 'solve_system_fast', 'computeRelativeRMSE', 'readTimeandApprox')
+           'solve_system_old', 'deleteValues', 'diag_add', 'generateSquareRandomMatrix', 'solve_system_fast',
+           'computeRelativeRMSE', 'readTimeandApprox', 'computeErrors')
 
 
 def createPlots(moments, fractions, title, name, xlabel, ylabel):
@@ -205,12 +207,27 @@ def compute_precision(Y_pred, Y, key):
 
 
 def compute_accuracy(Y_pred, Y, key):
+    """
+    returns the accuracy, given the predictions and the ground truth labels
+    @param Y_pred: predictions
+    @param Y: ground truth labels
+    @param key: 'val' or 'test' to print
+    @return: accuracy score
+    """
     accuracy = metrics.accuracy_score(Y, Y_pred)
     print(f"{key} accuracy: {accuracy * 100}%")
     return accuracy
 
 
 def print_accuracy(A, Kxvx, Y, key):
+    """
+    Computing the predictions and printing the accuracy
+    @param A: The inverse of Kxx
+    @param Kxvx: Kernel matrix between the training and validation/test data
+    @param Y: ground truth labels
+    @param key: ''validation' or 'test' used for the printing
+    @return:
+    """
     Ypred = (Kxvx @ A).argmax(dim=1)
     acc = metrics.accuracy_score(Y, Ypred)
     print(f"{key} accuracy: {acc * 100}%")
@@ -235,7 +252,7 @@ def computeRMSE(x: np.ndarray, y: np.ndarray, fraction) -> np.float:
     N = int(fraction * y.shape[0] * y.shape[1])
     diff = x - y
     diff = diff ** 2
-    return np.sqrt(np.sum(diff)) / N
+    return np.sqrt(np.sum(diff) / N)
 
 
 def computeRelativeRMSE(orig, approx, fraction) -> np.float:
@@ -331,3 +348,17 @@ def loadTargets(dataset):
     """
     _, Y = next(iter(DataLoader(dataset, batch_size=len(dataset))))
     return Y
+
+
+def computeErrors(x: np.ndarray, y: np.ndarray) -> Error:
+    """
+    Given two matrices computes the max, min and median error and returns an Error object containing them all
+    @param x: Original matrix
+    @param y: Approximated matrix
+    @return: Error object containing min, max and median error
+    """
+    diff = np.abs(x - y)
+    min_error = np.min(diff)
+    max_error = np.max(diff)
+    median_error = np.median(diff)
+    return Error(min_error, max_error, median_error)