6653bd874194081c6bc719a014a1708c3d9512db
ge25jez <martin.meinel@tum.de>
2020-10-15 17:39:48 +0200

    * added evaluation for using Nyström and Iterative SVD with flexible raction of the dataset

--- evaluation/evaluateIterativeSVD.py
+++ evaluation/evaluateIterativeSVD.py
@@ -1,4 +1,5 @@
 import os
+from timeit import default_timer as timer
 
 import fire
 import h5py
@@ -9,15 +10,17 @@ from utils import load_kern, loadTargets, solve_system_fast, oneHotEncoding, pri
 
 SVD_PATH = "./evaluation/svd.h5"
 SOL_PATH = "./evaluation/sol.h5"
+SVD_PATH_EVAL = './evaluation/eval.h5'
 
 
 def evaluateIterSVD(fraction=0.2):
-    os.system(f"python -m plotting.computeKernel computeValidationAndTestKernel {SVD_PATH}")
+    start = timer()
+    os.system(f"python -m plotting.computeKernel computeValidationAndTestKernel {SVD_PATH_EVAL}")
     os.system(f"python -m plotting.computeKernel computeKxxMatrix {SVD_PATH} Kxx_pert {fraction}")
     os.system(f"python -m plotting.computeMatrixFactorization {SVD_PATH} {SOL_PATH} svd")
     with h5py.File(SOL_PATH, 'r') as f:
         approx = np.array(f.get('approx'))
-    with h5py.File(SVD_PATH, 'r') as f:
+    with h5py.File(SVD_PATH_EVAL, 'r') as f:
         Kxvx = load_kern(f['Kxvx'], 0)
         Kxtx = load_kern(f['Kxtx'], 0)
     dataset = loadDataset()
@@ -29,6 +32,9 @@ def evaluateIterSVD(fraction=0.2):
     A = solve_system_fast(approx, oneHotEncoding(Y))
     print_accuracy(A, Kxvx, Yv, 'validation')
     print_accuracy(A, Kxtx, Yt, 'test')
+    end = timer()
+    diff = (end - start) / 60
+    print(diff)
 
 
 if __name__ == "__main__":
--- evaluation/evaluateNyström.py
+++ evaluation/evaluateNyström.py
@@ -1,4 +1,5 @@
 import os
+from timeit import default_timer as timer
 
 import fire
 import h5py
@@ -8,26 +9,31 @@ from plotting.createStartPlot import loadModel, loadDataset
 from utils import solve_system_fast, loadTargets, oneHotEncoding, print_accuracy, load_kern
 
 NYSTROM_PATH = "./evaluation/kernels.h5"
+NYSTROM_PATH_EVAL = './evaluation/nyst.h5'
 
 
 def evaluateNyström(fraction=0.2):
+    start = timer()
     model = loadModel()
     dataset = loadDataset()
     test = loadDataset(mode='test')
     val = loadDataset(mode='val')
     Y = loadTargets(dataset)
-    os.system(f"python -m plotting.computeKernelComputeValidationAndTestKernel {NYSTROM_PATH}")
+    os.system(f"python -m plotting.computeKernel computeValidationAndTestKernel {NYSTROM_PATH_EVAL}")
     components = int(fraction * len(dataset))
-    nyström = Nystroem(components, k=None, dataset=dataset, model=model, path=NYSTROM_PATH)
-    approximation = nyström.fit_transform()
+    nystroem = Nystroem(components, k=None, dataset=dataset, model=model, path=NYSTROM_PATH)
+    approximation = nystroem.fit_transform()
     A = solve_system_fast(Kxx=approximation, Y=oneHotEncoding(Y))
-    with h5py.File(NYSTROM_PATH, 'r') as f:
+    with h5py.File(NYSTROM_PATH_EVAL, 'r') as f:
+        Kxvx = load_kern(f['Kxvx'], 0)
         Kxtx = load_kern(f['Kxtx'], 0)
-        Kxvx = load_kern(f['Kxtx'], 0)
     Yt = loadTargets(test)
     Yv = loadTargets(val)
     print_accuracy(A, Kxvx, Yv, 'validation')
     print_accuracy(A, Kxtx, Yt, 'test')
+    end = timer()
+    diff = (end - start) / 60
+    print(diff)
 
 
 if __name__ == "__main__":
